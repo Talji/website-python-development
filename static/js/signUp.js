7947f2219175e7d316dcc726d74cf674b0b7d979
Talji <taljicodes@gmail.com>
2020-02-04 01:25:18 -0500

    First Commit

---
+++ static/js/signUp.js
@@ -0,0 +1,17 @@
+$(function() {
+    $('#btnSignUp').click(function() {
+console.log("This is working so far");
+
+        $.ajax({
+            url: '/signUp',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
