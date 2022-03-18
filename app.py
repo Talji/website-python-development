215da7c1ac35e2d3501c0137f8f783e1edcaa4ca
Talji <ssonic26@gmail.com>
2022-03-17 22:15:06 -0400

    All about Likes
    Implemented more Like functionality
    
    +Like Counter Added
    +Can now toggle Likes for Wishes
    +Likes now update without page refresh
    
    With this, we are now branching out into uncharted waters. We finished the tutorial huzzah!
    
    TODOs:
    
    - Implement Success Confirmation when Users Sign Up
    - Account for when an  email has  already been registered
    - Clean up site navigation links
    - Replace filler content on landing page.

--- app.py
+++ app.py
@@ -55,7 +55,16 @@ def addUpdateLike():
 
             if len(data) is 0:
                 conn.commit()
-                return json.dumps({'status':'OK'})
+                cursor.close()
+                conn.close()
+
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.callproc('sp_getLikeStatus',(_wishId,_user))
+                result = cursor.fetchall()
+
+
+                return json.dumps({'status':'OK', 'total':result[0][0], 'likeStatus':result[0][1]})
             else:
                 return render_template('error.html',error = 'An error occured!')
         else:
@@ -75,9 +84,10 @@ def getAllWishes():
     try:
         if session.get('user'):
 
+            _user = session.get('user')
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.callproc('sp_GetAllWishes')
+            cursor.callproc('sp_GetAllWishes',(_user,))
             result = cursor.fetchall()
 
             wishes_dict = []
@@ -86,7 +96,9 @@ def getAllWishes():
                         'Id': wish[0],
                         'Title': wish[1],
                         'Description': wish[2],
-                        'FilePath': wish[3]}
+                        'FilePath': wish[3],
+                        'Like': wish[4],
+                        'HasLiked': wish[5]}
                 wishes_dict.append(wish_dict)
 
             return  json.dumps(wishes_dict)
@@ -369,4 +381,4 @@ def signUp():
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
